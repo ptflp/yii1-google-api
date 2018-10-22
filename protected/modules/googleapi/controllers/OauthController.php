448c926db2c18a3b22d2fe26e0605274426c45d9
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-23 01:56:08 +0900

    google oauth complete

--- protected/modules/googleapi/controllers/OauthController.php
+++ protected/modules/googleapi/controllers/OauthController.php
@@ -2,15 +2,55 @@
 
 class OauthController extends Controller
 {
-	public $defaultAction='authorize';
+	public $defaultAction = 'authorize';
+
+	public $GoogleOauth;
+
+	public function actionAuthenticate()
+	{
+		if(!Yii::app()->user->isGuest){
+			$this->redirect(Yii::app()->homeUrl);
+		}
+
+		$this->setGoogleOauth();
+		$oauthUrl = $this->GoogleOauth
+				->createOauthLink()
+				->getOauthLink();
+
+		$this->redirect($oauthUrl);
+	}
 
 	public function actionAuthorize()
 	{
-		if(Yii::app()->user->isGuest){
+		$get = $this->getActionParams();
 
-				$this->redirect($oauthUrl);
-		  }else{
-				$this->redirect(Yii::app()->homeUrl);
-		  }
+		if(!Yii::app()->user->isGuest){
+			$this->redirect(Yii::app()->homeUrl);
+		}
+
+		if (!isset($get['code']) && !isset($get['scope'])) {
+			$this->redirect(Yii::app()->homeUrl);
+		}
+
+		$this->setGoogleOauth();
+		$userInfo = $this->GoogleOauth
+											->requestOauthToken($get['code'])
+											->requestUserInfo()
+											->getUserInfo();
+
+		$authorize = $this->container->get('UserAuthorize');
+		$authorize->setEmail($userInfo->email)->login();
+		$this->redirect(Yii::app()->homeUrl);
+	}
+
+	public function setGoogleOauth()
+	{
+		$configPath =Yii::app()->params['client_secrets'];
+		$this->GoogleOauth = $this->container
+				->get('GoogleOauth')
+				->loadFileConfig($configPath)
+				->setRedirectUri('http://localhost:8000/googleapi/oauth/authorize')
+				->addScope(GoogleOauth::USERINFO_EMAIL)
+				->addScope(GoogleOauth::USERINFO_PROFILE);
 	}
 }
