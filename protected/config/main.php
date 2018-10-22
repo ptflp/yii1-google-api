ebf9a6f8c3630bcaec6da4bd8599762f6c2aae95
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-22 23:26:25 +0900

    add module

--- protected/config/main.php
+++ protected/config/main.php
@@ -22,6 +22,7 @@ return array(
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
 		'admin',
+		'googleapi',
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'test',
