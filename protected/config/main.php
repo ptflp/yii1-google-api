11cdbd9a724f08ec1f5ef253e0bb7128b1ed7e58
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-22 18:28:47 +0900

    add authmanager component

--- protected/config/main.php
+++ protected/config/main.php
@@ -32,6 +32,12 @@ return array(
 
 	// application components
 	'components'=>array(
+		'authManager' => array(
+			// Будем использовать свой менеджер авторизации
+			'class' => 'PhpAuthManager',
+			// Роль по умолчанию. Все, кто не админы, модераторы и юзеры — гости.
+			'defaultRoles' => array('guest'),
+	   ),
 
 		'user'=>array(
 			// enable cookie-based authentication
