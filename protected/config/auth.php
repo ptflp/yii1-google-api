e5993bc2d0727dd9517125187798c57cd2d475fb
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-22 18:29:16 +0900

    define roles config

---
+++ protected/config/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+	'0' => array(
+		'type' => CAuthItem::TYPE_ROLE,
+		'description' => 'Guest',
+		'bizRule' => null,
+		'data' => null
+	),
+	'1' => array(
+		'type' => CAuthItem::TYPE_ROLE,
+		'description' => 'User',
+		'children' => array(
+			'guest', // унаследуемся от гостя
+		),
+		'bizRule' => null,
+		'data' => null
+	),
+	'999' => array(
+		'type' => CAuthItem::TYPE_ROLE,
+		'description' => 'Admin',
+		'children' => array(
+			'moderator',         // позволим админу всё, что позволено модератору
+		),
+		'bizRule' => null,
+		'data' => null
+	),
+);
