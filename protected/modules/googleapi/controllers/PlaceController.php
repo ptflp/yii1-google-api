f79065251271ab1ce5f6ca06baf3ef6b0cbe29d2
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-23 12:26:55 +0900

    move google components to googleapi module

--- protected/modules/googleapi/controllers/PlaceController.php
+++ protected/modules/googleapi/controllers/PlaceController.php
@@ -4,6 +4,41 @@ class PlaceController extends Controller
 {
 	public function actionIndex()
 	{
-		$this->render('index');
+		if(isset($_GET['city'])&&isset($_GET['place'])) {
+			$city = $_GET['city'];
+			$place = $_GET['place'];
+			$address = $place;
+			$jsonKey = file_get_contents(Yii::app()->params['g_api_key']);
+			$key = json_decode($jsonKey);
+			$key = $key->key;
+			$container = new DI\Container();
+			$gapi = $container->get('GooglePlacesApi');
+
+			$cityObj = $gapi
+						->setApiKey($key)
+						->requestCitiesByName($city)
+						->findOne()
+						->requestDetails('geometry,address_components')
+						->getResults();
+			$placesDetails = $gapi
+							->requestPlacesByCity($city,$place)
+							->requestDetails('geometry,address_components,types')
+							->getResults();
+			$addressDetails = $gapi
+								->requestAdressByCity($city,$address)
+								->requestDetails('geometry,address_components')
+								->getResults();
+			$params = [
+				'addressDetails'=>$addressDetails,
+				'placesDetails'=>$placesDetails,
+				'cityObj'=>$cityObj
+			];
+
+		} else {
+			$params = [];
+		}
+		echo '<pre>';
+		print_r($params);
+		echo '</pre>';
 	}
 }
