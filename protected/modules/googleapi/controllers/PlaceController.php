693a134cc389611c024455b32a51774670ce486c
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-23 22:41:55 +0900

    test place nearbysearch api

--- protected/modules/googleapi/controllers/PlaceController.php
+++ protected/modules/googleapi/controllers/PlaceController.php
@@ -5,31 +5,85 @@ class PlaceController extends Controller
 	public function actionSearch()
 	{
 		if(isset($_GET['city'])&&isset($_GET['keyword'])) {
-			$city = $_GET['city'];
-			$place = $_GET['keyword'];
+			$city = mb_strtolower($_GET['city']);
+			$place = mb_strtolower($_GET['keyword']);
 			$address = $place;
 			$jsonKey = file_get_contents(Yii::app()->params['g_api_key']);
 			$key = json_decode($jsonKey);
 			$key = $key->key;
 			$container = new DI\Container();
-			$gapi = $container->get('GooglePlacesApi');
+			$gapi = $container->get('GooglePlacesApi')
+									->setApiKey($key);
 
 			$cityObj = $gapi
-						->setApiKey($key)
 						->requestCitiesByName($city)
 						->findOne()
 						->requestDetails('geometry,address_components')
 						->getResults();
-			$placesDetails = $gapi
-							->nearbySearch($place,'store')
-							->getResults();
-			// $addressDetails = $gapi
-			// 					->requestAdressByCity($city,$address)
-			// 					->requestDetails('geometry,address_components')
-			// 					->getResults();
+
+			$placeTypes = [
+				[
+					'en' => 'movie_theater',
+					'ru' => 'кинотеатр'
+				],
+				[
+					'en' => 'lodging',
+					'ru' => 'гостиница'
+				],
+				[
+					'en' => 'cafe',
+					'ru' => 'кафе'
+				],
+				[
+					'en' => 'bar',
+					'ru' => 'бар'
+				],
+				[
+					'en' => 'liquor_store',
+					'ru' => 'Магазин спиртных напитков'
+				],
+				[
+					'en' => 'supermarket',
+					'ru' => 'супермаркет'
+				]
+			];
+
+
+			$places = [];
+			foreach ($placeTypes as $type) {
+				$temp = $gapi
+				->nearbySearch($type['ru'].' '.$place,$type['en'])
+				->getResults();
+				foreach ($temp as $item) {
+					$itemName = mb_strtolower($item->name);
+					if(strpos($itemName,$place) !== false){
+						$name = $item->name . ', ' . $type['ru'];
+						$lat = $item->geometry->location->lat;
+						$lng = $item->geometry->location->lng;
+						$address = $item->vicinity;
+						$places[] = [
+							"name" => $name,
+							"longitude" => $lng,
+							"latitude" => $lat,
+							"address" => $address
+						];
+						// "name": "Лена, кинотеатр",
+						// "longitude": 129.721214,
+						// "latitude": 62.020283,
+						// "address": "Ленина проспект, 45",
+					}
+				}
+			}
+			echo '<pre>';
+			print_r($places);
+			echo '</pre>';
+
+			$addressDetails = $gapi
+								->requestAdressByCity($city,$address)
+								->getResults();
 			$params = [
-				// 'addressDetails'=>$addressDetails,
-				'placesDetails'=>$placesDetails,
+				'addressDetails'=>$addressDetails,
+				'places'=>$places,
 				'cityObj'=>$cityObj
 			];
 
