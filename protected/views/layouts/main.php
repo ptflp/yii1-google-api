b9f9ea2d91d9222f5049dee553c66739768e7053
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-24 23:04:35 +0900

    creating template

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,59 +1,207 @@
-<?php /* @var $this Controller */ ?>
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<!--[if lte IE 9]> <html class="lte-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html lang="en"> <!--<![endif]-->
+
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="language" content="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Remove Tap Highlight on Windows Phone IE -->
+    <meta name="msapplication-tap-highlight" content="no"/>
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
-	<![endif]-->
+    <link rel="icon" type="image/png" href="/main/img/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="/main/img/favicon-32x32.png" sizes="32x32">
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
+    <title>Google API Yii 1.16</title>
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+
+    <!-- uikit -->
+    <link rel="stylesheet" href="bower_components/uikit/css/uikit.almost-flat.min.css" media="all">
+
+    <!-- altair admin -->
+    <link rel="stylesheet" href="/main/css/main.min.css" media="all">
+
+    <link href="jsoneditor/dist/jsoneditor.min.css" rel="stylesheet" type="text/css">
+    <style>
+         div#jsoneditorTree, div#jsoneditorCode {
+            height: 377px;
+         }
+    </style>
+
+    <!-- matchMedia polyfill for testing media queries in JS -->
+    <!--[if lte IE 9]>
+        <script type="text/javascript" src="bower_components/matchMedia/matchMedia.js"></script>
+        <script type="text/javascript" src="bower_components/matchMedia/matchMedia.addListener.js"></script>
+        <link rel="stylesheet" href="/main/css/ie.css" media="all">
+    <![endif]-->
+
 </head>
+<body class="disable_transitions sidebar_main_swipe">
+    <!-- main header -->
+    <header id="header_main">
+        <div class="header_main_content">
+            <nav class="uk-navbar">
 
-<body>
+                <div class="uk-navbar-flip">
+                    <ul class="uk-navbar-nav user_actions">
+                        <li><a href="#" class="user_action_icon uk-visible-large"><i class="material-icons md-24 md-light">fullscreen</i>asfasfas</a></li>
+                        <li data-uk-dropdown="{mode:'click',pos:'bottom-right'}">
+                            <a href="#" class="user_action_image"><img class="md-user-image" src="/main/img/avatars/avatar_11_tn.png" alt=""/></a>
+                            <div class="uk-dropdown uk-dropdown-small">
+                                <ul class="uk-nav js-uk-prevent">
+                                    <li><a href="page_user_profile.html">My profile</a></li>
+                                    <li><a href="page_settings.html">Settings</a></li>
+                                    <li><a href="login.html">Login Page</a></li>
+                                    <li><a href="login_v2.html">Login Page v2</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header><!-- main header end -->
+    <!-- main sidebar -->
+    <div id="page_content">
+        <div id="page_content_inner">
 
-<div class="container" id="page">
+            <div class="md-card">
+                <div class="md-card-content">
+                    <h3 class="heading_a">Input fields</h3>
+                    <div class="uk-grid" data-uk-grid-margin>
+                        <div class="uk-width-medium-1-1">
+                            <div class="uk-form-row">
+                                <div class="uk-grid" data-uk-grid-margin>
+                                    <div class="uk-width-medium-1-3">
+                                        <select id="select_demo_5" data-md-selectize data-md-selectize-bottom data-uk-tooltip="{pos:'top'}" title="Select with tooltip">
+                                            <option value="">Выберите город</option>
+                                            <option value="a">Item A</option>
+                                            <option value="b">Item B</option>
+                                            <option value="c">Item C</option>
+                                        </select>
+                                        <span class="uk-form-help-block">Список городов</span>
+                                    </div>
+                                    <div class="uk-width-medium-2-3">
+                                        <label>Введите адрес или место</label>
+                                        <input type="text" class="md-input" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="md-card">
+                <div class="md-card-content">
+                    <h3 class="heading_a">Input fields</h3>
+                    <div class="uk-grid" data-uk-grid-margin>
+                        <div class="uk-width-medium-1-2">
+                            <div id="jsoneditorCode"></div>
+                        </div>
+                        <div class="uk-width-medium-1-2">
+                            <div id="jsoneditorTree"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+        </div>
+    </div>
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Главная', 'url'=>array('/site/index', 'city'=>'якутск', 'place'=>'лена')),
-				array('label'=>'Админка', 'url'=>array('/admin/user'), 'visible'=>Yii::app()->user->checkAccess(User::ROLE_ADMIN)),
-				array('label'=>'Настройки', 'url'=>array('/user/settings'), 'visible'=>!Yii::app()->user->isGuest),
-				array('label'=>'Войти через Google', 'url'=>array('/googleapi/oauth/authenticate'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+    <!-- google web fonts -->
+    <script>
+        WebFontConfig = {
+            google: {
+                families: [
+                    'Source+Code+Pro:400,700:latin',
+                    'Roboto:400,300,500,700,400italic:latin'
+                ]
+            }
+        };
+        (function() {
+            var wf = document.createElement('script');
+            wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+        })();
+    </script>
 
-	<?php echo $content; ?>
+    <!-- common functions -->
+    <script src="/main/js/common.js"></script>
+    <!-- uikit functions -->
+    <script src="/main/js/uikit_custom.js"></script>
+    <!-- altair common functions/helpers -->
+    <script src="/main/js/altair_admin_common.js"></script>
+    <script src="jsoneditor/dist/jsoneditor.min.js"></script>
 
-	<div class="clear"></div>
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
+    <script>
+        $(function() {
+            if(isHighDensity()) {
+                $.getScript( "/main/js/custom/dense.min.js", function(data) {
+                    // enable hires images
+                    altair_helpers.retina_images();
+                });
+            }
+            if(Modernizr.touch) {
+                // fastClick (touch devices)
+                FastClick.attach(document.body);
+            }
+        });
+        $window.load(function() {
+            // ie fixes
+            altair_helpers.ie_fix();
+        });
+    </script>
 
-</div><!-- page -->
 
+   <script>
+     var job = {
+       "title": "Job description",
+       "type": "object",
+       "required": ["address"],
+       "properties": {
+         "company": {
+           "type": "string"
+         },
+         "role": {
+           "type": "string"
+         },
+         "address": {
+           "type": "string"
+         },
+         "salary": {
+           "type": "number",
+           "minimum": 120
+         }
+       }
+     };
+     var json = {
+       firstName: 'John',
+       lastName: 'Doe',
+       gender: null,
+       age: "28",
+       availableToHire: 1,
+       job: {
+         company: 'freelance',
+         role: 'developer',
+         salary: 100
+       }
+     };
+     var optionsCode = {
+       mode: 'code',
+     };
+     var optionsTree = {
+       mode: 'tree',
+     };
+     // create the editor
+     var container = document.getElementById('jsoneditorCode');
+     var editor = new JSONEditor(container, optionsCode, json);
+     var container = document.getElementById('jsoneditorTree');
+     var editor = new JSONEditor(container, optionsTree, json);
+   </script>
 </body>
 </html>
