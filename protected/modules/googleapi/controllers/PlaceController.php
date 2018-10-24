94c0e6313b59c7864ebbb6497dfc433c943796ea
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-24 11:09:17 +0900

    minor refactor

--- protected/modules/googleapi/controllers/PlaceController.php
+++ protected/modules/googleapi/controllers/PlaceController.php
@@ -5,14 +5,16 @@ class PlaceController extends Controller
 	public function actionSearch()
 	{
 		if(isset($_GET['city'])&&isset($_GET['keyword'])) {
+
 			$city = mb_strtolower($_GET['city']);
 			$place = mb_strtolower($_GET['keyword']);
 
 			$place = trim($place);
 
-			$placeSearch = $this->container->get('PlaceSearch');
-
-			$places = $placeSearch->getData($city,$place)->getResults();
+			$places = $this->container
+								->get('PlaceSearch')
+								->getData($city,$place)
+								->getResults();
 
 			$this->renderJSON($places);
 		}
