5d9ac95aa306595e9892a3cd73a633b8346bb4af
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-25 20:08:00 +0900

    templating user settings

--- protected/views/user/settings.php
+++ protected/views/user/settings.php
@@ -1,21 +1,30 @@
 <?php
 /* @var $this UserController */
 /* @var $model User */
-
-$this->breadcrumbs=array(
-	'Users'=>array('index'),
-	$model->id=>array('view','id'=>$model->id),
-	'Update',
-);
-
-$this->menu=array(
-	array('label'=>'List User', 'url'=>array('index')),
-	array('label'=>'Create User', 'url'=>array('create')),
-	array('label'=>'View User', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage User', 'url'=>array('admin')),
-);
 ?>
 
-<h1>Update User <?php echo $model->id; ?></h1>
+<h1>User settings</h1>
 
-<?php $this->renderPartial('_form', array('model'=>$model,'cityList'=>$cityList)); ?>
+    <div id="page_content">
+        <div id="page_content_inner">
+				<div class="uk-grid" data-uk-grid-margin>
+					<div class="uk-width-large-7-10">
+						<div class="md-card">
+								<div class="user_heading" data-uk-sticky="{ top: 48, media: 960 }">
+									<div class="user_heading_avatar fileinput fileinput-new" data-provides="fileinput">
+										<div class="fileinput-new thumbnail">
+												<img src="<?=Yii::app()->user->getAvatar()?>" alt="user avatar"/>
+										</div>
+									</div>
+									<div class="user_heading_content">
+										<h2 class="heading_b"><span class="uk-text-truncate" id="user_edit_uname"><?=Yii::app()->user->name?></span><span class="sub-heading" id="user_edit_position">Land acquisition specialist</span></h2>
+									</div>
+								</div>
+								<div class="user_content">
+								<?php $this->renderPartial('_form', array('model'=>$model,'cityList'=>$cityList)); ?>
+								</div>
+						</div>
+					</div>
+				</div>
+        </div>
+    </div>
