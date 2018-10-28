cd1954b74e15d3a42c974363e95590d9f7c6f388
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-29 00:42:18 +0900

    post only clear city table

--- protected/modules/admin/controllers/CityController.php
+++ protected/modules/admin/controllers/CityController.php
@@ -16,6 +16,7 @@ class CityController extends Controller
         return array(
             'accessControl', // perform access control for CRUD operations
             'postOnly + delete', // we only allow deletion via POST request
+            'postOnly + clear', // we only allow deletion via POST request
         );
     }
 
