a6431a0857b8958e4a3e978dd7655bdb47017a91
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-29 17:37:59 +0900

    cached by keys

--- protected/modules/googleapi/controllers/PlaceController.php
+++ protected/modules/googleapi/controllers/PlaceController.php
@@ -4,6 +4,36 @@ class PlaceController extends Controller
 {
 	public function actionSearch()
 	{
+    $placeTypes = [
+      [
+        'en' => 'movie_theater',
+        'ru' => 'кинотеатр'
+      ],
+      [
+        'en' => 'electronics_store',
+        'ru' => 'Магазин электроники'
+      ],
+      [
+        'en' => 'lodging', // сюда входят мини гостиницы и прочие
+        'ru' => 'гостиница'
+      ],
+      [
+        'en' => 'cafe',
+        'ru' => 'кафе'
+      ],
+      [
+        'en' => 'bar',
+        'ru' => 'бар'
+      ],
+      [
+        'en' => 'liquor_store',
+        'ru' => 'Магазин спиртных напитков'
+      ],
+      [
+        'en' => 'supermarket',
+        'ru' => 'супермаркет'
+      ]
+    ];
 		if(isset($_GET['city_id']) && isset($_GET['keyword'])) {
 			if(is_numeric($_GET['city_id']) && mb_strlen($_GET['keyword'])>1) {
 				$matchPercent = 61.8;
@@ -29,7 +59,35 @@ class PlaceController extends Controller
         // $this->debug($addressRaw);
         $redis = $this->cache;
         foreach ($placesRaw as $item) {
-          $key = 'c:'.$cityId.':p:'.$item->place_id;
+          $itemName = mb_strtolower($item->name);
+          $words = explode(' ', $place);
+          if (count($words) == 2) {
+            $key0 = array_search($words[0], array_column($placeTypes, 'ru'));
+            $key1 = array_search($words[1], array_column($placeTypes, 'ru'));
+          }
+          $input = $place;
+          if (is_int($key0)) {
+            $input = $words[1];
+          }
+          if (is_int($key1)) {
+            $input = $words[0];
+          }
+          similar_text($itemName, $input, $percent);
+          if($percent > $matchPercent) {
+            if (mb_strlen($place)<10) {
+              $keyword = base64_encode($_GET['keyword']);
+            } else {
+              $keyword = md5($_GET['keyword']);
+            }
+            $keyList = 'c:'.$cityId.':q:'.$keyword;
+
+            $rlist=$redis->lrange($keyList, 0, -1);
+            $search = array_search($item->place_id,$rlist);
+            if (!is_int($search)) {
+              $redis->rpush($keyList, $item->place_id);
+            }
+          }
+          $key = $item->place_id;
           $name = $item->name . ', ' . $item->types[0];
           $lat = $item->geometry->location->lat;
           $lng = $item->geometry->location->lng;
@@ -41,10 +99,11 @@ class PlaceController extends Controller
             "address" => $address
           ]);
         }
+        $rlist=$redis->lrange($keyList, 0, -1);
+        $this->debug($rlist);
         $data = [];
-        foreach ($placesRaw as $item) {
-          $key = 'c:'.$cityId.':p:'.$item->place_id;
-          $data[] = $redis->hgetall($key);
+        foreach ($rlist as $item) {
+          $data[] = $redis->hgetall($item);
         }
         $this->debug($data);
         // $this->debug($placesRaw);
@@ -54,7 +113,10 @@ class PlaceController extends Controller
 				$this->renderJSON([]);
 			}
 		} else {
-			$this->renderJSON([]);
+      echo mb_strlen($_GET['keyword']);
+      $tmp = base64_encode($_GET['keyword']);
+      echo "$tmp dlina ".strlen($tmp);
+			// $this->renderJSON([]);
 		}
 	}
 
