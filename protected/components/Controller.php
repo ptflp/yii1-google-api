5cfd6becf661b0c540afddccc023156e71a7f6b3
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-30 22:56:57 +0900

    fix PSR

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -5,50 +5,50 @@
  */
 class Controller extends CController
 {
-	/**
-	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
-	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
-	 */
-	public $layout='//layouts/main';
-	/**
-	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
-	 */
-	public $menu=array();
-	/**
-	 * @var array the breadcrumbs of the current page. The value of this property will
-	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
-	 * for more details on how to specify this property.
-	 */
-	public $breadcrumbs=array();
+    /**
+     * @var string the default layout for the controller view. Defaults to '//layouts/column1',
+     * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
+     */
+    public $layout='//layouts/main';
+    /**
+     * @var array context menu items. This property will be assigned to {@link CMenu::items}.
+     */
+    public $menu=array();
+    /**
+     * @var array the breadcrumbs of the current page. The value of this property will
+     * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
+     * for more details on how to specify this property.
+     */
+    public $breadcrumbs=array();
 
-  public $container;
+    public $container;
 
-  public $cache;
+    public $cache;
 
-	protected function beforeAction($action)
-	{
-    $this->container = include __DIR__ . '/../config/php-di.php';
-		return parent::beforeAction($action);
-	}
+    protected function beforeAction($action)
+    {
+        $this->container = include __DIR__ . '/../config/php-di.php';
+        return parent::beforeAction($action);
+    }
 
-	protected function renderJSON($data)
-	{
-		header('Content-type: application/json');
-		// echo CJSON::encode($data);
-		echo json_encode($data,JSON_UNESCAPED_UNICODE);
+    protected function renderJSON($data)
+    {
+        header('Content-type: application/json');
+        // echo CJSON::encode($data);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
 
-		foreach (Yii::app()->log->routes as $route) {
-			if($route instanceof CWebLogRoute) {
-					$route->enabled = false; // disable any weblogroutes
-			}
-		}
-		Yii::app()->end();
-	}
+        foreach (Yii::app()->log->routes as $route) {
+            if ($route instanceof CWebLogRoute) {
+                    $route->enabled = false; // disable any weblogroutes
+            }
+        }
+        Yii::app()->end();
+    }
 
-	protected function debug($data)
-	{
-		echo '<pre>';
-		print_r($data);
-		echo '</pre>';
-	}
+    protected function debug($data)
+    {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
 }
