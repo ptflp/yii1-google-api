aaff833a28b1e9fc7795662c8503b34ddaa31911
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-31 22:20:58 +0900

    fix texteditor

--- web/main/js/pages/page_snippets.js
+++ web/main/js/pages/page_snippets.js
@@ -31,56 +31,5 @@ altair_snippets = {
                 );
             });
 
-        var snippet_CodeMirror = UIkit.htmleditor($html_editor, {
-            'toolbar': '',
-            'height': '240'
-        }).editor;
-
-        // save snippet
-        $('#snippet_new_save').on('click',function() {
-
-            snippet_CodeMirror.save();
-            var language = $("#snippet_language").val(),
-                title = $("#snippet_title").val(),
-                content = snippet_CodeMirror.getValue();
-
-            // close main modal
-            $snippet_modal.find('.uk-modal-close').click();
-
-            // uncomment code bellow to save snippet to json
-            UIkit.modal.confirm('Do you want to save this snippet: <strong>"' + title + '"</strong>?', function(){
-                UIkit.modal.alert("Snipped saved!");
-                // uncoment following code to activate snippets saving
-                /*$.ajax({
-                    type: "POST",
-                    url: "./data/save_snippet.php",
-                    data: {
-                        language: language,
-                        title: title,
-                        content: content
-                    },
-                    dataType: 'text',
-                    success: function(){
-                        // clear fields
-                        $("#snippet_language,#snippet_title").val('');
-                        snippet_CodeMirror.setValue('');
-                        // show message
-                        UIkit.modal.alert("Snipped saved!");
-                    }
-                });
-                */
-            });
-
-
-        });
-
-        // refresh htmleditor on modal show
-        $snippet_modal.on({
-            'show.uk.modal': function(){
-                $("#snippet_title").focus();
-                snippet_CodeMirror.refresh();
-            }
-        });
-
     }
 };
