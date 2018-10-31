52b3a650090a405cfe9184b01575200f5e27d288
Petr Filippov <filippov.pa@s-vfu.ru>
2018-10-31 15:50:53 +0900

    searching синет

--- protected/modules/googleapi/controllers/PlaceController.php
+++ protected/modules/googleapi/controllers/PlaceController.php
@@ -64,4 +64,74 @@ class PlaceController extends Controller
             $this->renderJSON([]);
         }
     }
+
+    public function actionTest()
+    {
+        $jsonKey = file_get_contents(Yii::app()->params['g_api_key']);
+        $keyObj = json_decode($jsonKey);
+        $key = $keyObj->key;
+
+        $client = $this->container->get('Modules\GoogleApi\ClientAdaptor');
+
+        $client->setUrl('https://maps.googleapis.com/maps/api/place/nearbysearch/json');
+
+        $types = [
+            "administrative_area_level_1",
+            "administrative_area_level_2",
+            "administrative_area_level_3",
+            "administrative_area_level_4",
+            "administrative_area_level_5",
+            "colloquial_area",
+            "country",
+            "establishment",
+            "finance",
+            "floor",
+            "food",
+            "general_contractor",
+            "geocode",
+            "health",
+            "intersection",
+            "locality",
+            "natural_feature",
+            "neighborhood",
+            "place_of_worship",
+            "political",
+            "point_of_interest",
+            "post_box",
+            "postal_code",
+            "postal_code_prefix",
+            "postal_code_suffix",
+            "postal_town",
+            "premise",
+            "room",
+            "route",
+            "street_address",
+            "street_number",
+            "sublocality",
+            "sublocality_level_4",
+            "sublocality_level_5",
+            "sublocality_level_3",
+            "sublocality_level_2",
+            "sublocality_level_1",
+            "subpremise"
+        ];
+
+        foreach ($types as $type) {
+            echo $type;
+            $paramsUrl = [
+                'location'=>"62.0354523,129.6754745",
+                'radius'=>17000,
+                'type'=>$type,
+                'language'=>"ru",
+                'keyword'=>"синет",
+                'key'=> $key,
+            ];
+
+            $client->setParamsUrl($paramsUrl);
+            $client->fetch();
+            $json = $client->getResponse();
+            $data = json_decode($json, true);
+            $this->debug($data);
+        }
+    }
 }
