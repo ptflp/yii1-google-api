fe81cbc7ce96fd06de92f7fe5fb6cbd90b1f0eb5
Petr Filippov <filippov.pa@s-vfu.ru>
2018-11-01 13:59:34 +0900

    fix command queue

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ docker-compose up -d
 echo 'Wait for db initialization'
 sleep 30s
 docker exec g-api-db mysql -proot -e "create database googleApi"
-docker exec -it g-api-app composer install
-docker exec -it g-api-app /app/protected/yiic migrate --interactive=0
+docker exec g-api-app composer install
 docker exec g-api-app bash ./fix_perm.sh
+docker exec g-api-app /app/protected/yiic migrate --interactive=0
